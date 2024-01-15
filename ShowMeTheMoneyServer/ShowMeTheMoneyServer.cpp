03afc51812786d9d826fb44b98b3db079de67bd9
Suha Do <weqa123@naver.com>
2024-01-15 09:49:14 +0900

    Make server and send client show me the money.

--- ShowMeTheMoneyServer/ShowMeTheMoneyServer.cpp
+++ ShowMeTheMoneyServer/ShowMeTheMoneyServer.cpp
@@ -1,6 +1,18 @@
 
-int main(int argc, char* argv[])
+#include <iostream>
+
+#include "Network/TCPServer.h"
+
+int main()
 {
-    
+    TcpServer* Server = new TcpServer();
+    Server->Initialize();
+    Server->Bind(5001);
+    Server->Listen();
+    const TcpSocket* ClientSocket = Server->Accept();
+    ClientSocket->Send("show me the money.");
+    const std::string Message = ClientSocket->Receive();
+    std::cout << Message << std::endl;
+
     return 0;
 }
